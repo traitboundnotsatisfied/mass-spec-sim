e3b7ec3001a55fae8ee3499c12abc153a1434a34
traitboundnotsatisfied <144365472+traitboundnotsatisfied@users.noreply.github.com>
2024-05-18 12:27:39 -0400

    okay some progress

---
+++ setup.js
@@ -0,0 +1,49 @@
+canvas.width = canvas_parent.getBoundingClientRect().width;
+canvas.height = canvas_parent.getBoundingClientRect().height;
+
+var ctx = canvas.getContext("2d");
+
+ctx.w = canvas.width;
+ctx.h = canvas.height;
+ctx.clearAll = () => ctx.clearRect(0, 0, ctx.w, ctx.h);
+ctx.fillCircle = (x, y, r) => {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI, false);
+    ctx.fill();
+};
+ctx.isInBounds = (arr, sc) => {
+    let x = (ctx.w / 2) + (arr[1] * sc);
+    let y = (ctx.h / 2) + (arr[1] * sc);
+    return (x >= 0) && (y >= 0) && (x <= ctx.w) && (y <= ctx.h);
+};
+
+var defaultCfg = {
+    type: "single-sector",
+    dt_s: 0.01,
+    particleTypes: [
+        {
+            attrs: {
+                name: "Na+",
+                color: "red",
+                mass_amu: 22.990,
+                charge: 1,
+            },
+            count: 10,
+        },
+        {
+            attrs: {
+                name: "K+",
+                color: "blue",
+                mass_amu: 39.098,
+                charge: 1,
+            },
+            count: 10,
+        },
+    ],
+};
+
+var mass_kg = particle => particle.attrs.mass_amu * 1.66054e-27;
+
+var badType = () => {
+    throw new Error("Bad type");
+};
--- simulate.js
+++ simulate.js
@@ -1,19 +1,69 @@
-let x;
+let particles;
+let animate;
 
-function reset() {
-    x = 0;
+const SCALE = 500;
+const DRAW_RADIUS = 5;
+
+function simulate(cfg) {
+    particles = [];
+    for (const particleType of cfg.particleTypes) {
+        for (let i = 0; i < particleType.count; i++) {
+            particles.push({
+                attrs: particleType.attrs,
+                pos: startingPosition_m(cfg.type, i / (particleType.count - 1)),
+                vel: [0, 0],
+                active: true,
+            });
+        }
+    }
+    animate = () => {
+        render();
+        let someActive = false;
+        for (let i = 0; i < particles.length; i++) {
+            if (particles[i].active) {
+                someActive = true;
+                let force_N = computeForce_N(cfg.type, particles[i]);
+                for (let j = 0; j < 2; j++) {
+                    particles[i].vel[j] += (force_N[j] / mass_kg(particles[i])) * cfg.dt_s;
+                    particles[i].pos[j] += particles[i].vel[j] * cfg.dt_s;
+                }
+                particles[i].active &&= isActive(cfg.type, particles[i].pos);
+                particles[i].active &&= ctx.isInBounds(particles[u].pos);
+            }
+        }
+        if (someActive) requestAnimationFrame(animate); else render();
+    };
+    animate();
 }
 
-function update() {
-    x += 1;
+function startingPosition_m(type, _prog) {switch (type) {
+    case "single-sector":
+        return [0, 0];
+}; badType()}
+
+function computeForce_N(type, particle) {switch (type) {
+    case "single-sector":
+        return [0, 1e-27];
+}; badType()}
+
+function isActive(type, pos) {switch (type) {
+    case "single-sector":
+        return 
+}; badType()}
+
+function drawParticle(particle) {
+    ctx.fillStyle = particle.attrs.color;
+    ctx.fillCircle(
+        Math.floor((ctx.w / 2) + (particle.pos[0] * SCALE)),
+        Math.floor((ctx.h / 2) + (particle.pos[1] * SCALE)),
+        DRAW_RADIUS,
+    );
 }
 
-function simulate() {
-    if (x < ctx.w) {
-        ctx.fillStyle = "white";
-        ctx.fillCircle(x, ctx.h / 2, 20);
-        console.log(x);
-        update();
-        requestAnimationFrame(simulate);
+function render() {
+    console.log(particles[0].pos);
+    ctx.clearAll();
+    for (const particle of particles) {
+        drawParticle(particle);
     }
 }
